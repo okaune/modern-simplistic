50d8de68901ca4cd9b5ba5a033ac790d0a79016c
Ole Kristian Aune <okaune@outlook.com>
2015-06-27 01:11:14 +0200

    Transparent bakgrunn på profile, endret noe js for å fungere riktig med det nye designet, endret design på footer mm.

--- index.php
+++ index.php
@@ -4,24 +4,7 @@
 		<h1 class="dropshadow">Jeg studerer for øyeblikket lysdesign ved Romerike folkehøgskole, men har også jobbet mye med webdesign ved siden av studiene. Jeg er veldig interresert i å følge med på nye trender innen webutvikling, men også innen design generelt.</h1>
 		<a class="more" href="#profile">Mer</a>
 
-		<div class="front-menu">
-			<a href="blog.php">
-				<div class="blog">
-					<svg class="icon">
-  						<use xlink:href="#icon-pencil" />
-					</svg>
-					Blogg
-				</div>
-			</a>
-			<a href="gallery.php">
-				<div class="gallery">
-					<svg class="icon">
-  						<use xlink:href="#icon-images" />
-					</svg>
-					Galleri
-				</div>
-			</a>
-		</div>
+		<?php include_once("module/module_front_menu.php"); ?>
 	</section>
 	<?php include_once("module/module_profile.php"); ?>
 	<?php include_once("elements/footer.php"); ?>
