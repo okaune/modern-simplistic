cc6d553fa5f39d4750b4cc6752bc65eca057ca13
Ole Kristian Aune <okaune@outlook.com>
2015-06-28 19:11:25 +0200

    Fikset noen includes og andre småting på blogg siden

--- blog.php
+++ blog.php
@@ -1,9 +1,8 @@
-<?php include_once("elements/head.php"); ?>
-	<?php include_once("elements/header.php"); ?>
-	<p>Let's just face it! Jeg kommer nok knapt til å skrive noe her, men jeg har nå ihvertfall en blogg, så hvis jeg skulle ha lyst til å dele noe, er det her det vil dukke opp. Vet ikke hva jeg kommer til å skrive om her, men det blir vel en god blanding av mye forskjellig som ikke her noen sammenheng :)</p>
-	
-	<!-- Set up a blog or use github as static blog -->
-	
-	<?php include_once("elements/footer.php"); ?>
-	
-<?php include_once("elements/foot.php"); ?>
+<?php include_once("templates/template_header.php"); ?>
+<!-- Initiate header witch class name -->
+<?php headerClass("blog"); ?>
+<div class="blog_wrapper row">
+	<!-- dummy text for now -->
+	<p class="column">Let's just face it! Jeg kommer nok knapt til å skrive noe her, men jeg har nå ihvertfall en blogg, så hvis jeg skulle ha lyst til å dele noe, er det her det vil dukke opp. Vet ikke hva jeg kommer til å skrive om her, men det blir vel en god blanding av mye forskjellig som ikke her noen sammenheng :)</p>
+</div>
+<?php include_once("templates/template_footer.php"); ?>
